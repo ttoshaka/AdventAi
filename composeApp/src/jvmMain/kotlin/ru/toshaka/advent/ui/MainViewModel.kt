64d0cb2c1f87fb544f47d15eca8c09704777deef
Antipov Anton <AAntipov@it-one.ru>
2025-12-02 13:20:31 +0400

    Попытка встроить koog

--- composeApp/src/jvmMain/kotlin/ru/toshaka/advent/ui/MainViewModel.kt
+++ composeApp/src/jvmMain/kotlin/ru/toshaka/advent/ui/MainViewModel.kt
@@ -1,13 +1,32 @@
 package ru.toshaka.advent.ui
-
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.core.agent.config.AIAgentConfig
+import ai.koog.agents.core.agent.functionalStrategy
+import ai.koog.agents.core.dsl.builder.forwardTo
+import ai.koog.agents.core.dsl.builder.strategy
+import ai.koog.agents.core.dsl.extension.*
+import ai.koog.agents.ext.agent.chatAgentStrategy
+import ai.koog.agents.features.eventHandler.feature.EventHandler
+import ai.koog.agents.snapshot.feature.Persistence
+import ai.koog.agents.snapshot.providers.file.FilePersistenceStorageProvider
+import ai.koog.prompt.dsl.Prompt
+import ai.koog.prompt.executor.clients.deepseek.DeepSeekLLMClient
+import ai.koog.prompt.executor.clients.deepseek.DeepSeekModels
+import ai.koog.prompt.executor.llms.SingleLLMPromptExecutor
+import ai.koog.rag.base.files.JVMFileSystemProvider
 import androidx.compose.ui.graphics.Color
 import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.actor
-import kotlinx.coroutines.flow.*
-import ru.toshaka.advent.data.agent.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import ru.toshaka.advent.data.agent.Agent
+import ru.toshaka.advent.data.agent.AiResponse
+import ru.toshaka.advent.data.agent.DeepSeekChatAgent
+import ru.toshaka.advent.data.agent.Summarizer
 import ru.toshaka.advent.data.db.AppDatabase
 import ru.toshaka.advent.data.db.agent.AgentEntity
 import ru.toshaka.advent.data.db.agent.AgentRepository
@@ -18,6 +37,8 @@ import ru.toshaka.advent.data.db.message.MessagesRepository
 import ru.toshaka.advent.data.db.message.isUser
 import ru.toshaka.advent.mcp.McpManager
 import java.io.File
+import java.net.URI
+import java.nio.file.Paths
 
 class MainViewModel {
 
@@ -34,6 +55,71 @@ class MainViewModel {
 
     private val summarizerAgent = Agent(Summarizer)
 
+    val client = DeepSeekLLMClient("sk-c388a56e101b4d989e16d87c6c080658")
+    val promptExecutor = SingleLLMPromptExecutor(client)
+    val agentConfig = AIAgentConfig(
+        prompt = Prompt.build("simple") {
+            system(
+                """
+               Ты ии-ассистент
+                """.trimIndent()
+            )
+        },
+        model = DeepSeekModels.DeepSeekChat,
+        maxAgentIterations = 10
+    )
+
+    val persistence = FilePersistenceStorageProvider(
+        fs = JVMFileSystemProvider.ReadWrite,
+        root = File("chat_state").toPath()
+    )
+
+    val agentName = "firstAgent4"
+    val agentStrategy = strategy(
+        name = "Simple calculator"
+    ) {
+        val nodeSendInput by nodeLLMRequest()
+        val nodeExecuteTool by nodeExecuteTool()
+        val nodeSendToolResult by nodeLLMSendToolResult()
+
+        edge(nodeStart forwardTo nodeSendInput)
+        edge(
+            (nodeSendInput forwardTo nodeFinish)
+                    transformed { it }
+                    onAssistantMessage { true }
+        )
+        edge(
+            (nodeSendInput forwardTo nodeExecuteTool)
+                    onToolCall { true }
+        )
+        edge(nodeExecuteTool forwardTo nodeSendToolResult)
+        edge(
+            (nodeSendToolResult forwardTo nodeFinish)
+                    transformed { it }
+                    onAssistantMessage { true }
+        )
+    }
+    val agent = AIAgent(
+        id = agentName,
+        promptExecutor = promptExecutor,
+        strategy = agentStrategy,
+        agentConfig = agentConfig,
+        installFeatures = {
+            install(EventHandler) {
+                onAgentStarting { eventContext ->
+                    println("Starting agent: ${eventContext.agent.id}")
+                }
+                onAgentCompleted { eventContext ->
+                    println("Result: ${eventContext.result}")
+                }
+            }
+            install(Persistence) {
+                storage = persistence
+                enableAutomaticPersistence = false
+            }
+        }
+    )
+
     /** Последовательное выполнение команд */
     @OptIn(ObsoleteCoroutinesApi::class)
     private val commandExecutor = scope.actor<suspend () -> Unit>(
@@ -44,33 +130,37 @@ class MainViewModel {
     }
 
     init {
-        scope.launch {
-            mcpManager.launchServer()
-        }
         observeChatsAndMessages()
     }
 
     private fun observeChatsAndMessages() = scope.launch {
-        combine(
-            chatRepo.getAllAsFlow(),
-            messageRepo.getAllAsFlow(),
-            agentRepo.getAllAsFlow()
-        ) { chats, messages, agents ->
-            _state.value = MainScreenState(
-                chats = chats.map { chat -> buildChatState(chat, messages, agents) },
-                availableAgents = agents.map { agent ->
-                    MainScreenState.Agent(
-                        name = agent.name,
-                        systemPrompt = agent.systemPrompt,
-                        id = agent.id,
-                        temperature = agent.temperature,
-                        maxTokens = agent.maxTokens,
-                    )
-                },
-                onSaveAgent = ::onSaveAgent,
-                onSaveChat = ::onSaveChat,
+
+        val checkPoint = persistence.getCheckpoints(agentName)
+        println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! $checkPoint ${checkPoint.firstOrNull()?.messageHistory}")
+        val mess = checkPoint.firstOrNull()?.messageHistory?.map {
+            MainScreenState.Chat.Message(
+                author = "qwe",
+                content = it.content,
+                position = MainScreenState.Chat.Message.Position.LEFT,
+                debug = "",
+                color = Color.Gray
             )
-        }.collect()
+        }
+        _state.value = MainScreenState(
+            chats = listOf(
+                MainScreenState.Chat(
+                    id = 999,
+                    name = "Чатец",
+                    messages = mess ?: emptyList(),
+                    onSendClick = { onUserMessage(999, it, emptyList()) },
+                    onClearClick = {},
+                    onSummarizeClick = {}
+                )
+            ),
+            availableAgents = emptyList(),
+            onSaveAgent = ::onSaveAgent,
+            onSaveChat = ::onSaveChat,
+        )
     }
 
     private fun onSummarizeClick(chatId: Long) {
@@ -174,17 +264,8 @@ class MainViewModel {
 
     private fun onUserMessage(chatId: Long, content: String, agents: List<AgentEntity>) {
         scope.launch {
-            saveMessage(
-                MessageEntity(
-                    chatId = chatId,
-                    owner = 0L,
-                    content = content,
-                    debugInfo = null,
-                    timestamp = System.currentTimeMillis(),
-                    history = true,
-                )
-            )
-            agents.forEach { request(chatId, it) }
+            agent.run(content)
+            observeChatsAndMessages()
         }
     }
 
